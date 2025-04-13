cbc0f88d2dc04b2121fa9874ed7985ee338ced81
Chris Lockard <github@chrislockard.net>
2025-04-12 22:42:31 -0400

    Add goodbye and buy endpoint

--- hello.go
+++ hello.go
@@ -4,9 +4,15 @@ import (
 	"net/http"
 )
 
-func main () {
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
+	})
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Goodbye, World!"))
+	})
+	http.HandleFunc("/buy", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Buy, World!"))
 	})
 	http.ListenAndServe(":8088", nil)
 }
